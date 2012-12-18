39e0af6b2f0ed72522e5b5a1bcb383caa3ee9869
Ray <gliese.q@gmail.com>
2012-12-18 11:56:59 +0800

    rename category to group0, kind to group1;add pedestrian, tram, monorail, subway group0; add casing2, inline2;

--- highway_road.sql
+++ highway_road.sql
@@ -1,26 +1,30 @@
 ﻿BEGIN;
 
 -- reversed order required
-DROP VIEW IF EXISTS brick_road_z10;
-DROP VIEW IF EXISTS brick_road_z11;
-DROP VIEW IF EXISTS brick_road_z12;
-DROP VIEW IF EXISTS brick_road_z13;
-DROP VIEW IF EXISTS brick_road_z14;
-DROP VIEW IF EXISTS brick_road_z15;
-DROP VIEW IF EXISTS brick_road_rail;
+DROP VIEW IF EXISTS roadhierarchy_z10;
+DROP VIEW IF EXISTS roadhierarchy_z11;
+DROP VIEW IF EXISTS roadhierarchy_z12;
+DROP VIEW IF EXISTS roadhierarchy_z13;
+DROP VIEW IF EXISTS roadhierarchy_z14;
+DROP VIEW IF EXISTS roadhierarchy_z15;
+DROP VIEW IF EXISTS roadhierarchy;
 
 
 
 
-CREATE VIEW  brick_road_rail AS
+CREATE VIEW  roadhierarchy AS
 	SELECT * FROM (
 		SELECT  
 			(CASE	WHEN highway IN ('motorway', 'motorway_link') THEN 'highway'
 				WHEN highway IN ('trunk', 'trunk_link') THEN 'trunk'
 				WHEN highway IN ('primary', 'primary_link', 'secondary', 'secondary_link', 'tertiary', 'tertiary_link') THEN 'major_road'
 				WHEN highway IN ('residential', 'unclassified', 'road', 'minor') THEN 'minor_road'
-				WHEN highway IN ('service', 'track', 'steps', 'path', 'cycleway', 'footpath', 'footway', 'pedestrian') THEN 'path'
-				WHEN railway IN ('rail', 'tram', 'light_rail', 'narrow_guage', 'monorail') THEN 'rail'
+				WHEN highway IN ('service', 'track', 'path', 'cycleway') THEN 'path'
+				WHEN highway IN ('steps', 'footpath', 'footway', 'pedestrian') THEN 'pedestrian'
+				WHEN railway IN ('rail') THEN 'rail'
+				WHEN railway IN ('tram', 'light_rail', 'narrow_gauge') THEN 'tram'
+				WHEN railway IN ('monorail') THEN 'monorail'
+				WHEN railway IN ('subway') THEN 'subway'
 				ELSE NULL END
 			) AS z15,
 			(CASE  WHEN highway IN ('motorway', 'motorway_link') THEN 'highway'
@@ -86,7 +90,9 @@ CREATE VIEW  brick_road_rail AS
 			COALESCE("highway", "railway") AS brick_kind,
 			(CASE
 				WHEN highway IN ('motorway') THEN 0
-			        WHEN railway IN ('rail', 'tram', 'light_rail', 'narrow_guage', 'monorail') THEN .5
+			    WHEN railway IN ('rail') THEN .5
+			    WHEN railway IN ('tram', 'light_rail', 'narrow_guage') THEN .6
+			    WHEN railway IN ('monorail') THEN .7
 				WHEN highway IN ('trunk') THEN 1
 				WHEN highway IN ('primary') THEN 2
 				WHEN highway IN ('secondary') THEN 3
@@ -94,6 +100,7 @@ CREATE VIEW  brick_road_rail AS
 				WHEN highway LIKE '%_link' THEN 5
 				WHEN highway IN ('residential', 'unclassified', 'road') THEN 6
 				WHEN highway IN ('service', 'minor') THEN 7
+				WHEN railway IN ('subway') THEN 8
 				ELSE 99 END
 			) AS priority,
 			* 
@@ -101,152 +108,207 @@ CREATE VIEW  brick_road_rail AS
 	) AS foo;
 
 
-CREATE VIEW brick_road_z15 AS
+CREATE VIEW roadhierarchy_z15 AS
 	SELECT  render, 
-		z15 as category, 
-		brick_kind as kind, 
+		z15 as group0, 
+		brick_kind as group1, 
 		brick_oneway as oneway, 
 		is_link,
 		is_tunnel,
 		is_bridge,
 		way
 	FROM (
-		SELECT *, 'outline' AS render, 1 AS is_outline, 1 AS is_casing, 0 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'outline' AS render, 1 AS is_outline, NULL::integer AS is_casing, NULL::integer AS is_inline
+		FROM roadhierarchy
 		WHERE z15 IS NOT NULL
 		UNION ALL
-		SELECT *, 'casing' AS render, 0 AS is_outline, 1 AS is_casing, 0 AS is_marker
-		FROM brick_road_rail	
+		SELECT *, 'casing1' AS render, NULL::integer AS is_outline, 1 AS is_casing, NULL::integer AS is_inline
+		FROM roadhierarchy	
 		WHERE z15 IS NOT NULL
 		UNION ALL
-		SELECT *, 'inline' AS render, 0 AS is_outline, 0 AS is_casing, 0 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'casing2' AS render, NULL::integer AS is_outline, 2 AS is_casing, NULL::integer AS is_inline
+		FROM roadhierarchy	
 		WHERE z15 IS NOT NULL
 		UNION ALL
-		SELECT *, 'marker' AS render, 0 AS is_outline, 0 AS is_casing, 1 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'inline1' AS render, NULL::integer AS is_outline, NULL::integer AS is_casing, 1 AS is_inline
+		FROM roadhierarchy	
+		WHERE z15 IS NOT NULL
+		UNION ALL
+		SELECT *, 'inline2' AS render, NULL::integer AS is_outline, NULL::integer AS is_casing, 2 AS is_inline
+		FROM roadhierarchy
 		WHERE z15 IS NOT NULL
 	) AS FOO
-	ORDER BY is_outline DESC, explicit_layer ASC, implied_layer ASC, is_casing DESC, is_marker ASC, priority DESC;
+	ORDER BY 
+	    is_outline ASC NULLS LAST, 
+	    explicit_layer ASC, 
+	    implied_layer ASC, 
+	    is_casing ASC NULLS LAST, 
+	    is_inline ASC NULLS LAST,
+	    priority DESC;
 
-CREATE VIEW brick_road_z14 AS
+CREATE VIEW roadhierarchy_z14 AS
 	SELECT  render, 
-		z14 as category, 
-		brick_kind as kind, 
+		z14 as group0, 
+		brick_kind as group1, 
 		brick_oneway as oneway, 
 		is_link,
 		is_tunnel,
 		is_bridge,
 		way
 	FROM (
-		SELECT *, 'outline' AS render, 1 AS is_outline, 1 AS is_casing, 0 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'outline' AS render, 1 AS is_outline, NULL::integer AS is_casing, NULL::integer AS is_inline
+		FROM roadhierarchy
 		WHERE z14 IS NOT NULL
 		UNION ALL
-		SELECT *, 'casing' AS render, 0 AS is_outline, 1 AS is_casing, 0 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'casing1' AS render, NULL::integer AS is_outline, 1 AS is_casing, NULL::integer AS is_inline
+		FROM roadhierarchy	
 		WHERE z14 IS NOT NULL
 		UNION ALL
-		SELECT *, 'inline' AS render, 0 AS is_outline, 0 AS is_casing, 0 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'casing2' AS render, NULL::integer AS is_outline, 2 AS is_casing, NULL::integer AS is_inline
+		FROM roadhierarchy	
 		WHERE z14 IS NOT NULL
 		UNION ALL
-		SELECT *, 'marker' AS render, 0 AS is_outline, 0 AS is_casing, 1 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'inline1' AS render, NULL::integer AS is_outline, NULL::integer AS is_casing, 1 AS is_inline
+		FROM roadhierarchy	
+		WHERE z14 IS NOT NULL
+		UNION ALL
+		SELECT *, 'inline2' AS render, NULL::integer AS is_outline, NULL::integer AS is_casing, 2 AS is_inline
+		FROM roadhierarchy
 		WHERE z14 IS NOT NULL
 	) AS FOO
-	ORDER BY is_outline DESC, explicit_layer ASC, implied_layer ASC, is_casing DESC, is_marker ASC, priority DESC;
+	ORDER BY
+		is_outline ASC NULLS LAST, 
+	    explicit_layer ASC, 
+	    implied_layer ASC, 
+	    is_casing ASC NULLS LAST, 
+	    is_inline ASC NULLS LAST,
+	    priority DESC;
 
-CREATE VIEW brick_road_z13 AS
+CREATE VIEW roadhierarchy_z13 AS
 	SELECT  render, 
-		z13 as category, 
-		brick_kind as kind, 
+		z13 as group0, 
+		brick_kind as group1, 
 		brick_oneway as oneway, 
 		is_link,
 		is_tunnel,
 		is_bridge,
 		way
 	FROM (
-		SELECT *, 'outline' AS render, 1 AS is_outline, 1 AS is_casing, 0 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'outline' AS render, 1 AS is_outline, NULL::integer AS is_casing, NULL::integer AS is_inline
+		FROM roadhierarchy
 		WHERE z13 IS NOT NULL
 		UNION ALL
-		SELECT *, 'casing' AS render, 0 AS is_outline, 1 AS is_casing, 0 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'casing1' AS render, NULL::integer AS is_outline, 1 AS is_casing, NULL::integer AS is_inline
+		FROM roadhierarchy	
 		WHERE z13 IS NOT NULL
 		UNION ALL
-		SELECT *, 'inline' AS render, 0 AS is_outline, 0 AS is_casing, 0 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'casing2' AS render, NULL::integer AS is_outline, 2 AS is_casing, NULL::integer AS is_inline
+		FROM roadhierarchy	
 		WHERE z13 IS NOT NULL
 		UNION ALL
-		SELECT *, 'marker' AS render, 0 AS is_outline, 0 AS is_casing, 1 AS is_marker
-		FROM brick_road_rail
+		SELECT *, 'inline1' AS render, NULL::integer AS is_outline, NULL::integer AS is_casing, 1 AS is_inline
+		FROM roadhierarchy	
+		WHERE z13 IS NOT NULL
+		UNION ALL
+		SELECT *, 'inline2' AS render, NULL::integer AS is_outline, NULL::integer AS is_casing, 2 AS is_inline
+		FROM roadhierarchy
 		WHERE z13 IS NOT NULL
 	) AS FOO
-	ORDER BY is_outline DESC, explicit_layer ASC, implied_layer ASC, is_casing DESC, is_marker ASC, priority DESC;
+	ORDER BY
+	    is_outline ASC NULLS LAST, 
+	    explicit_layer ASC, 
+	    implied_layer ASC, 
+	    is_casing ASC NULLS LAST, 
+	    is_inline ASC NULLS LAST,
+	    priority DESC;
 
 
-CREATE VIEW brick_road_z12 AS
+CREATE VIEW roadhierarchy_z12 AS
 	SELECT  render, 
-		z12 as category, 
-		brick_kind as kind, 
+		z12 as group0, 
+		brick_kind as group1, 
 		brick_oneway as oneway, 
 		is_link,
 		is_tunnel,
 		is_bridge,
 		way
 	FROM (
-		SELECT *, 'outline' AS render, 1 AS is_outline
-		FROM brick_road_rail
+		SELECT *, 'outline' AS render, 1 AS is_outline, NULL::integer AS is_inline
+		FROM roadhierarchy
 		WHERE z12 IS NOT NULL
 		UNION ALL
-		SELECT *, 'inline' AS render, 0 AS is_outline
-		FROM brick_road_rail
+		SELECT *, 'inline1' AS render, NULL::integer AS is_outline, 1 AS is_inline
+		FROM roadhierarchy	
+		WHERE z12 IS NOT NULL
+		UNION ALL
+		SELECT *, 'inline2' AS render, NULL::integer AS is_outline, 2 AS is_inline
+		FROM roadhierarchy
 		WHERE z12 IS NOT NULL
 	) AS FOO
-	ORDER BY is_outline DESC, explicit_layer ASC, implied_layer ASC,priority DESC;
+	ORDER BY 	    
+	    is_outline ASC NULLS LAST, 
+	    explicit_layer ASC, 
+	    implied_layer ASC, 
+	    is_inline ASC NULLS LAST,
+	    priority DESC;
 
 
-CREATE VIEW brick_road_z11 AS
+CREATE VIEW roadhierarchy_z11 AS
 	SELECT  render, 
-		z11 as category, 
-		brick_kind as kind, 
+		z11 as group0, 
+		brick_kind as group1, 
 		brick_oneway as oneway, 
 		is_link,
 		is_tunnel,
 		is_bridge,
 		way
 	FROM (
-		SELECT *, 'outline' AS render, 1 AS is_outline
-		FROM brick_road_rail
+		SELECT *, 'outline' AS render, 1 AS is_outline, NULL::integer AS is_inline
+		FROM roadhierarchy
 		WHERE z11 IS NOT NULL
 		UNION ALL
-		SELECT *, 'inline' AS render, 0 AS is_outline
-		FROM brick_road_rail
+		SELECT *, 'inline1' AS render, NULL::integer AS is_outline, 1 AS is_inline
+		FROM roadhierarchy	
+		WHERE z11 IS NOT NULL
+		UNION ALL
+		SELECT *, 'inline2' AS render, NULL::integer AS is_outline, 2 AS is_inline
+		FROM roadhierarchy
 		WHERE z11 IS NOT NULL
 	) AS FOO
-	ORDER BY is_outline DESC, explicit_layer ASC, implied_layer ASC,priority DESC;
+	ORDER BY
+	    is_outline ASC NULLS LAST, 
+	    explicit_layer ASC, 
+	    implied_layer ASC, 
+	    is_inline ASC NULLS LAST,
+	    priority DESC;
 
 
-CREATE VIEW brick_road_z10 AS
+CREATE VIEW roadhierarchy_z10 AS
 	SELECT  render, 
-		z10 as category, 
-		brick_kind as kind, 
+		z10 as group0, 
+		brick_kind as group1, 
 		brick_oneway as oneway, 
 		is_link,
 		is_tunnel,
 		is_bridge,
 		way
 	FROM (
-		SELECT *, 'outline' AS render, 1 AS is_outline
-		FROM brick_road_rail
+		SELECT *, 'outline' AS render, 1 AS is_outline, NULL::integer AS is_inline
+		FROM roadhierarchy
 		WHERE z10 IS NOT NULL
 		UNION ALL
-		SELECT *, 'inline' AS render, 0 AS is_outline
-		FROM brick_road_rail
+		SELECT *, 'inline1' AS render, NULL::integer AS is_outline, 1 AS is_inline
+		FROM roadhierarchy	
+		WHERE z10 IS NOT NULL
+		UNION ALL
+		SELECT *, 'inline2' AS render, NULL::integer AS is_outline, 2 AS is_inline
+		FROM roadhierarchy
 		WHERE z10 IS NOT NULL
 	) AS FOO
-	ORDER BY is_outline DESC, explicit_layer ASC, implied_layer ASC,priority DESC;
-
-	
+	ORDER BY 
+	    is_outline ASC NULLS LAST, 
+	    explicit_layer ASC, 
+	    implied_layer ASC, 
+	    is_inline ASC NULLS LAST,
+	    priority DESC;
 END;
